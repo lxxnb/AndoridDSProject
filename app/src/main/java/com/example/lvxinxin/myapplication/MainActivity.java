418299ce4f62ff6642128560e9c2aadee5342c27
lxxnb <18513426687@163.com>
2018-11-27 15:28:09 +0800

    修改代码

--- app/src/main/java/com/example/lvxinxin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lvxinxin/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.lvxinxin.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * sdf
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
